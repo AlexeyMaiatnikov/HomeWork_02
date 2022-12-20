71d75fe56230f349a2b37054db41b7d5305bc169
[AlexeyMaiatnikov] <heldinbox87@gmail.com>
2022-12-20 16:19:08 +0800

    add Task03

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -5,3 +5,22 @@
 7 -> да
 1 -> нет
 */
+using static System.Console;
+Clear();
+WriteLine("Введите число от 1 до 7: ");
+int day_number = Convert.ToInt32(ReadLine());
+if (day_number <= 7 && day_number >= 1)
+{
+    if (day_number >= 6)
+    {
+        WriteLine("Да");
+    }
+    if (day_number <= 5)
+    {
+        WriteLine("Нет");
+    }
+}
+else
+{
+    WriteLine("Недопустимое число");
+}
