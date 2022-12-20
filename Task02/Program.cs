a060bd69d7bace18a3a09a06a40ab9b4f6938f7a
[AlexeyMaiatnikov] <heldinbox87@gmail.com>
2022-12-20 16:08:27 +0800

    add Task02

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -4,3 +4,25 @@
 78 -> третьей цифры нет
 32679 -> 6
 */
+using static System.Console;
+Clear();
+Write("Введите число: ");
+int number = Convert.ToInt32(ReadLine());
+//int b = number;
+if (number < 100)
+{
+    WriteLine("Третьей цифры нет ");
+};
+if (number >= 100 & number <= 999)
+{
+    Write($"{number % 10}");
+};
+while (number > 1000)
+{
+    number = number / 10;
+    if (number < 1000)
+    {
+        WriteLine($"{number % 10}");
+    };
+
+}
